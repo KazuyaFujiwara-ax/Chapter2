aca6121872373dd51f79236f9b829475872ee485
藤原和矢 <kazuya.fujiwara@axas-japan.co.jp>
2022-02-07 00:13:24 +0900

    array, dict, optional

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,43 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//print test
+var greeting: String = "Hello, playground"
 1+2
-"こんにちは"
-print("Hello," + "playground")
+print(greeting)
 
+var apple: Int = 6
+print("りんごが\(apple)個")
+//¥と\が違う文字って、嘘だろ？
+
+
+//Int type
+let imax: Int = Int.max
+let imin: Int = Int.min
+
+print("INT_MAX: \(imax)\nINT_MIN: \(imin)")
+
+
+//array
+var nums: [Int] = [1, 2, 3, 6]
+nums[0]
+nums[1]
+nums[2]
+nums[3]
+
+
+//dictionary type
+var dict: [String:String] = ["Mac":"Apple","Windows":"Microsoft"]
+dict["iOS"] = "Apple"
+
+dict["Mac"]
+dict["Windows"]
+dict["iOS"]
+
+
+//Optional type, unwrap
+var opStr : String?
+opStr = "not nil"
+
+if let unwrappedStr = opStr {
+    print(unwrappedStr)
+}
